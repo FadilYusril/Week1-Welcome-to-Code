2c9d78d655cd11b1899c055ed1c3dad65f808493
FadilYusril <yusrilneneknu51@gmail.com>
2025-05-19 14:49:18 +0800

    Ini dulu bang, looping masih agak bingung

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,28 @@
+// ## Soal 4. Breaking Sentence (yet Again) and Count Each Length
+// js
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let secondWord4 = word4.substring(4, 14);
+let secondWordLenght = secondWord4.length;
+console.log(`Second Word: ` + secondWord4 + `, with lenght: ` + secondWordLenght);
+
+let thirdWord4 = word4.substring(15, 17);
+let thirdWordLenght = thirdWord4.length;
+console.log(`Third Word: ` + thirdWord4 + `, with lenght: ` + thirdWordLenght);
+
+let fourthWord4 = word4.substring(18, 20);
+let fourthWordLenght = fourthWord4.length;
+console.log(`Fourth Word: ` + fourthWord4 + `, with lenght: ` + fourthWordLenght);
+
+let fifthWord4 = word4.substring(21, 25);
+let fifthWordLenght = fifthWord4.length;
+console.log(`Fifth Word: ` + fifthWord4 + `, with lenght: ` + fifthWordLenght);
