8060fa16f3e056b50faf9282d9fa44a4c7ce680a
FadilYusril <yusrilneneknu51@gmail.com>
2025-05-20 21:50:53 +0800

    push ujian week1 phase0

---
+++ quiz/ujian/soal1.js
@@ -0,0 +1,32 @@
+// # Ujian
+
+// ***DISINI KALIAN AKAN MEMBUAT ALGORITMA KALIAN SENDIRI SESUAI PERMASALAHAN YANG ADA DIBAWAH***
+// ***KERJAKAN DENGAN SESUAI TEST-CASE NYA***
+
+
+// ## Soal 1
+// ```js
+// // Problem
+// // Diberikan sebuah function bandingkanAngka(angka1, angka2) yang menerima dua parameter angka. 
+// // Function akan me-return nilai true jika angka2 lebih besar dari angka1, dan false jika sebaliknya. 
+// // Jika kedua angka bernilai sama, function akan me-return -1.
+
+function bandingkanAngka(angka1, angka2) {
+    if (angka1 < angka2) {
+        return true
+    } else if (angka1 > angka2) {
+        return false
+    } else {
+        return -1
+    }
+  //code disini
+}
+  
+//   // TEST CASES
+  console.log(bandingkanAngka(5, 8)); // true
+  console.log(bandingkanAngka(5, 3)); // false
+  console.log(bandingkanAngka(4, 4)); // -1
+  console.log(bandingkanAngka(3, 3)); // -1
+  console.log(bandingkanAngka(17, 2)); // false
+
+// ``
