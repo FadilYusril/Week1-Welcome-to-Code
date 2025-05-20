eb9a699f897b7caaa90e82ed7a8abb769d4dd0c0
FadilYusril <yusrilneneknu51@gmail.com>
2025-05-20 13:51:50 +0800

    part3nya kelupaan hehe

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,20 @@
+// ## Soal 5. Bintang asteriks
+// js
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+
+let output = "";
+for (let i = 1; i <= input; i++) {
+for (let j = 1; j <= i; j++) {
+output += "*";
+}
+output += "\n";
+}
+console.log(output)
